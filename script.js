ffca5e3baffbab4ff168f25083ea154758de6531
Pritam333555 <168451589+Pritam333555@users.noreply.github.com>
2024-04-29 20:08:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('myForm');
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	// Add your form submission logic here
+	alert('Form submitted successfully!');
+});
